782b55ba18318dbe37acac0c58749fccea18221e
Lucas Nelaupe <lucas34990@gmail.com>
2015-10-02 16:22:14 +0200

    Fetch data from server

--- app/src/main/java/com/nelaupe/qanda/fragment/MainFragment.java
+++ app/src/main/java/com/nelaupe/qanda/fragment/MainFragment.java
@@ -8,20 +8,21 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.gson.reflect.TypeToken;
 import com.nelaupe.qanda.R;
-import com.nelaupe.qanda.entity.Answer;
 import com.nelaupe.qanda.entity.Question;
-import com.nelaupe.qanda.entity.User;
+import com.nelaupe.qanda.rest.RequestServer;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+
+import bolts.Continuation;
+import bolts.Task;
 
 /**
  * Created with IntelliJ
@@ -31,11 +32,14 @@ import java.util.List;
 public class MainFragment extends BaseFragment {
 
     private QuestionAdapter mAdapter;
+    private Task<List<Question>> mLoader;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        RequestServer<List<Question>> requestServer = new RequestServer<>(new TypeToken<List<Question>>(){}); // Stupid java
+        mLoader = requestServer.doLoad("questions");
     }
 
     @Override
@@ -61,43 +65,61 @@ public class MainFragment extends BaseFragment {
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mAdapter = new QuestionAdapter(fetchData());
+        mAdapter = new QuestionAdapter();
         mRecyclerView.setAdapter(mAdapter);
 
+        mLoader.continueWith(new Continuation<List<Question>, Object>() {
+            @Override
+            public Object then(Task<List<Question>> task) throws Exception {
+                mAdapter.addAll(task.getResult());
+                return null;
+            }
+        });
+
     }
 
-    private List<Question> fetchData() {
-//        Fetch from server later.
-//        Dummy data
-
-        ArrayList<Question> result = new ArrayList<>();
-
-        for (int i = 0; i < 50; i++) {
-            Question question = new Question();
-            question.id = i;
-            question.title = "What do you think of this app so far ?";
-            question.user = new User();
-            question.user.username = "Lucas";
-            question.date = new Date();
-            question.answers = new ArrayList<>();
-
-            Answer answer = new Answer();
-            answer.id = 0;
-            answer.content = "I Like it";
-
-            question.answers.add(answer);
-            result.add(question);
-        }
-
-        return result;
-    }
+//    private List<Question> fetchData() {
+////        Fetch from server later.
+////        Dummy data
+//
+//        ArrayList<Question> result = new ArrayList<>();
+//
+//        for (int i = 0; i < 50; i++) {
+//            Question question = new Question();
+//            question.id = i;
+//            question.title = "What do you think of this app so far ?";
+//            question.user = new User();
+//            question.user.username = "Lucas";
+//            question.date = new Date();
+//            question.answers = new ArrayList<>();
+//
+//            Answer answer = new Answer();
+//            answer.id = 0;
+//            answer.content = "I Like it";
+//
+//            question.answers.add(answer);
+//            result.add(question);
+//        }
+//
+//        return result;
+//    }
 
     public class QuestionAdapter extends RecyclerView.Adapter<QuestionViewHolder> {
 
         private List<Question> mQuestions;
 
+        public QuestionAdapter() {
+            this.mQuestions = new ArrayList<>();
+        }
+
         public QuestionAdapter(List<Question> questions) {
             this.mQuestions = questions;
+        }
+
+        public void addAll(List<Question> questions) {
+            mQuestions.clear();
+            mQuestions.addAll(questions);
+            notifyDataSetChanged();
         }
 
         @Override
@@ -110,7 +132,7 @@ public class MainFragment extends BaseFragment {
             Question question = mQuestions.get(i);
             questionViewHolder.vTitle.setText(question.title);
             questionViewHolder.vUser.setText(question.user.username);
-            questionViewHolder.vDate.setText(DateUtils.getRelativeTimeSpanString(question.date.getTime()));
+//            questionViewHolder.vDate.setText(DateUtils.getRelativeTimeSpanString(question.date.getTime()));
         }
 
         @Override
---
+++ app/src/main/java/com/nelaupe/qanda/rest/RequestServer.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright
+ */
+package com.nelaupe.qanda.rest;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.TextHttpResponseHandler;
+
+import java.lang.reflect.Type;
+
+import bolts.Task;
+import cz.msebera.android.httpclient.Header;
+
+/**
+ * Created with IntelliJ
+ * Created by lucas
+ * Date 26/03/15
+ */
+public class RequestServer<TSelf> {
+
+    private static final String BASE_URL = "http://qanda.lucas-nelaupe.fr/";
+    private static AsyncHttpClient client = new AsyncHttpClient();
+    private Type mAnswerType;
+
+
+    public RequestServer (TypeToken type) {
+        mAnswerType = type.getType();
+    }
+
+    public Task<TSelf> doLoad(final String url)
+    {
+        final Task<TSelf>.TaskCompletionSource source = Task.create();
+        client.get(getAbsoluteUrl(url), null, new TextHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                TSelf response = new GsonBuilder().create().fromJson(responseString, mAnswerType);
+                source.trySetResult(response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                source.trySetError(new Exception(throwable));
+            }
+
+        });
+        return source.getTask();
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+
+}
